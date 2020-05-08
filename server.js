845864e99f17a7cc3ba2288e93e0927bac95c840
lmachens <leon.machens@googlemail.com>
2020-05-08 20:58:30 +0200

    Allow dynamic PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const io = require("socket.io")(http);
 const { secrets } = require("./secrets");
 const path = require("path");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const games = {};
 const players = {};
