bede0475436e9fbb7c81d7860152cb61877b1f5d
lmachens <leon.machens@googlemail.com>
2020-05-11 14:29:17 +0200

    Proxy websocket in development

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/",
+    createProxyMiddleware({
+      target: "http://localhost:8080",
+      changeOrigin: true,
+      ws: true,
+    })
+  );
+};
