d6a9600345777fb4949370eed13f4572cf7fd19b
lmachens <leon.machens@googlemail.com>
2020-05-11 14:41:31 +0200

    Fix websocket proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/",
+    "/socket.io",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "http://localhost:8080/socket.io",
       changeOrigin: true,
       ws: true,
     })
